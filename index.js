0eea35b0c3055cebf1d8aeaff2ab4f9c48ac83a9
kevin del cid <danieldelcidf@gmail.com>
2023-05-10 20:04:01 -0600

    Add redux and navigator

---
+++ index.js
@@ -0,0 +1,23 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './src/App';
+import {name as appName} from './app.json';
+
+import { Provider } from 'react-redux'
+import { store } from './src/Store';
+
+
+
+const AppInitPoint = () => (
+    
+          <Provider store={store}>
+            <App /> 
+          </Provider>
+
+  );
+
+
+AppRegistry.registerComponent(appName, () => AppInitPoint);
